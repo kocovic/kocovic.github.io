b2390247dd75a16c528ac91d7008136bf57c5d23
kocovic <kocovic.github.io>
2018-09-28 13:04:42 +0200

    CV

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(window).on('scroll', function() {
+    if($(window).scrollTop()) {
+        $('#nav-wrap').addClass('black');
+    }
+    else{
+        $('#nav-wrap').removeClass('black');
+    }
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#go-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#go-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#go-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
